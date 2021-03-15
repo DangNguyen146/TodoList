b4cdab266df030a1306f8dc0ad1d4e804256d8d9
DangNguyen146 <19521317@gm.uit.edu.vn>
2021-03-15 14:38:25 +0700

    TodoList

--- js/taskService.js
+++ js/taskService.js
@@ -1,34 +1,34 @@
 function taskService() {
-    this.getListUserApi = function() {
-        return axios({
-            url: 'https://5fd46f50e9cda40016f5bf35.mockapi.io/api/taskCV',
-            method: "GET",
-        });
-    };
-    this.deleteUserApi = function(id) {
-        return axios({
-            url: `https://5fd46f50e9cda40016f5bf35.mockapi.io/api/taskCV/${id}`,
-            method: "DELETE",
-        })
-    };
-    this.addUserApi = function(user) {
-        return axios({
-            url: 'https://5fd46f50e9cda40016f5bf35.mockapi.io/api/taskCV',
-            method: "POST",
-            data: user,
-        })
-    };
-    this.getTaskById = function(id) {
-        return axios({
-            url: `https://5fd46f50e9cda40016f5bf35.mockapi.io/api/taskCV/${id}`,
-            method: "GET",
-        })
-    }
-    this.updateUserApi = function(user) {
-        return axios({
-            url: `https://5fd46f50e9cda40016f5bf35.mockapi.io/api/taskCV/${user.id}`,
-            method: "PUT",
-            data: user,
-        })
-    };
+  this.getListUserApi = function () {
+    return axios({
+      url: "https://5fd46f50e9cda40016f5bf35.mockapi.io/api/taskCV",
+      method: "GET",
+    });
+  };
+  this.deleteUserApi = function (id) {
+    return axios({
+      url: `https://5fd46f50e9cda40016f5bf35.mockapi.io/api/taskCV/${id}`,
+      method: "DELETE",
+    });
+  };
+  this.addUserApi = function (user) {
+    return axios({
+      url: "https://5fd46f50e9cda40016f5bf35.mockapi.io/api/taskCV",
+      method: "POST",
+      data: user,
+    });
+  };
+  this.getTaskById = function (id) {
+    return axios({
+      url: `https://5fd46f50e9cda40016f5bf35.mockapi.io/api/taskCV/${id}`,
+      method: "GET",
+    });
+  };
+  this.updateUserApi = function (user) {
+    return axios({
+      url: `https://5fd46f50e9cda40016f5bf35.mockapi.io/api/taskCV/${user.id}`,
+      method: "PUT",
+      data: user,
+    });
+  };
 }
